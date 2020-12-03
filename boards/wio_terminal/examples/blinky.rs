584c8f7726c856fae3c84d5990577fe4ebe89b2c
Tom <twitchyliquid64@ciphersink.net>
2020-12-03 09:48:55 -0800

    WIP systick timer

--- boards/wio_terminal/examples/blinky.rs
+++ boards/wio_terminal/examples/blinky.rs
@@ -5,7 +5,7 @@ use panic_halt as _;
 use wio_terminal as wio;
 
 use wio::hal::clock::GenericClockController;
-use wio::hal::delay::Delay;
+use wio::hal::systick_delay;
 use wio::pac::{CorePeripherals, Peripherals};
 use wio::prelude::*;
 use wio::{entry, Pins, Sets};
@@ -22,14 +22,18 @@ fn main() -> ! {
         &mut peripherals.OSCCTRL,
         &mut peripherals.NVMCTRL,
     );
-    let mut delay = Delay::new(core.SYST, &mut clocks);
+    //let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    let delay_builder = init_systick_delay(core.SYST, &mut clocks);
 
     let mut sets: Sets = Pins::new(peripherals.PORT).split();
     let mut user_led = sets.user_led.into_open_drain_output(&mut sets.port);
     user_led.set_low().unwrap();
 
     loop {
+        delay_builder.delay_ms(250);
         user_led.toggle();
-        delay.delay_ms(200u8);
     }
 }
+
+systick_delay!(SYSTICK_TIMER);
--- hal/src/common/delay.rs
+++ hal/src/common/delay.rs
@@ -7,7 +7,12 @@ use crate::clock::GenericClockController;
 use crate::time::Hertz;
 use hal::blocking::delay::{DelayMs, DelayUs};
 
-/// System timer (SysTick) as a delay provider
+use core::sync::atomic::{
+    AtomicU32,
+    Ordering::Relaxed,
+};
+
+/// System timer (SysTick) as a polling-driven delay provider
 pub struct Delay {
     sysclock: Hertz,
     syst: SYST,
@@ -86,4 +91,113 @@ impl DelayUs<u8> for Delay {
     fn delay_us(&mut self, us: u8) {
         self.delay_us(us as u32)
     }
+}
+
+
+/// Static controller for an Interrupt-driven System timer (SysTick) with
+/// a 1ms interval.
+pub struct MultiDelayController {
+    syst: core::cell::UnsafeCell<SYST>,
+    wraps: core::cell::UnsafeCell<AtomicU32>, // Should increment every 1ms.
+}
+
+impl MultiDelayController {
+    /// Configures the system timer (SysTick) as a delay provider
+    pub fn new<'a>(mut syst: SYST, clocks: &mut GenericClockController) -> Self {
+        let sysclock: Hertz = clocks.gclk0().into();
+        syst.disable_counter();
+        syst.disable_interrupt();
+        syst.set_clock_source(SystClkSource::Core);
+
+        let ticks_per_ms = sysclock.0 / 1_000;
+        syst.set_reload(ticks_per_ms);
+        syst.clear_current();
+
+        Self {
+            syst: core::cell::UnsafeCell::new(syst),
+            wraps: core::cell::UnsafeCell::new(AtomicU32::new(0)),
+        }
+    }
+
+    #[doc(hidden)]
+    pub fn handle_interrupt(&self) {
+        unsafe {
+            if (*self.syst.get()).has_wrapped() {
+                (*self.wraps.get()).fetch_add(1, Relaxed);
+            }
+        }
+    }
+
+    #[doc(hidden)]
+    pub unsafe fn enable(&mut self) {
+        (*self.syst.get()).enable_interrupt();
+        (*self.syst.get()).enable_counter();
+    }
+
+    /// Returns the number of 16ms intervals that have occurred.
+    fn get_wraps(&self) -> u32 {
+        unsafe {
+            (*self.wraps.get()).load(Relaxed)
+        }
+    }
+
+    pub fn delay_ms(&self, num: u32) {
+        let start_wraps = self.get_wraps();
+        while num > 0 && self.get_wraps() != start_wraps.wrapping_add(num) {
+            cortex_m::asm::wfi();
+        }
+    }
+}
+
+// /// Cloneable type, each of which lets you delay independently.
+// #[derive(Clone,Copy)]
+// pub struct MultiDelayBuilder<'a> {
+//     alias: core::ptr::NonNull<MultiDelayController>,
+//     pd: core::marker::PhantomData<&'a ()>,
+// }
+//
+// impl<'a> MultiDelayBuilder<'a> {
+//     pub fn wait_lol(&self) -> u32 {
+//         unsafe {
+//             self.alias.as_ref().wraps.load(Relaxed)
+//         }
+//     }
+// }
+
+#[macro_export]
+macro_rules! systick_delay {
+    ($name:ident) => {
+            use cortex_m::interrupt as _systick_interrupt;
+            use cortex_m::peripheral::SYST as _systick_syst;
+            use atsamd_hal::clock::GenericClockController as _systick_ClockController;
+            use atsamd_hal::delay as _systick_delay;
+
+            static mut $name: Option<_systick_delay::MultiDelayController> = None;
+
+            fn init_systick_delay<'a>(syst: _systick_syst, clocks: &mut _systick_ClockController) -> &'a _systick_delay::MultiDelayController { // -> _systick_delay::MultiDelayBuilder {
+                _systick_interrupt::free(|_| {
+                    // No need to check if already initialized as we consume a required
+                    // value (SYST).
+                    let controller = _systick_delay::MultiDelayController::new(syst, clocks);
+                    unsafe {
+                        $name = Some(controller)
+                    };
+                    unsafe {
+                        $name.as_mut().map(|s| s.enable())
+                    };
+                    unsafe {
+                        $name.as_ref().unwrap()
+                    }
+                })
+            }
+
+
+            #[cortex_m_rt::exception]
+            #[allow(non_upper_case_globals, unused_unsafe)]
+            unsafe fn SysTick() {
+                _systick_interrupt::free(|_| {
+                    $name.as_ref().map(|s| s.handle_interrupt())
+                });
+            }
+    };
 }
